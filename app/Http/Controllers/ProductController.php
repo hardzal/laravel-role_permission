dc5d38f0f94e9a07ca0b414387b380ed717d33fc
hardzal <suryadijogja@gmail.com>
2021-08-26 15:51:34 +0700

    add ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function __construct()
+    {
+
+        $this->middleware('permission:product-list|product-create|product-edit|product-delete', ['only' => ['index', 'show']]);
+        $this->middleware('permission:product-create', ['only' => ['create', 'store']]);
+        $this->middleware('permission:product-edit', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:product-delete', ['only' => ['destroy']]);
+    }
+
+    public function index()
+    {
+        $products = Product::latest()->paginate(5);
+
+        return view('products.index', compact('products'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        request()->validate([
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+
+        Product::create($request->all());
+
+        return redirect()->route('products.index')
+            ->with('success', 'Product created succesfully');
+    }
+
+    public function show(Product $product)
+    {
+        return view('products.edit', compact('products'));
+    }
+
+    public function edit(Product $product)
+    {
+        return view('products.edit', compact('products'));
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        request()->validate([
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+
+        $product->update($request->all());
+
+        return redirect()->route('products.index')
+            ->with('success', 'Product updated succesfully');
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return redirect()->route('products.index')
+            ->with('success', 'Product deleted successfully');
+    }
 }
